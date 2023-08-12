309cec3fd4cab6ad1889d9e7becf24323756d08b
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-12 16:42:19 -0300

    Ejercicio_5

---
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1/Ejercicio_5.java
@@ -0,0 +1,37 @@
+package trabajo_practico_1;
+
+/*Realizá un programa que permita ingresar valores del mismo tipo para las variables num1 y num2.
+ *  Una vez cargadas, mostrar ambas variables por pantalla, intercambiá sus valores 
+ *  (que lo cargado en num1 quede en num2, y viceversa) y volvé a mostrarlas actualizadas. [EC]
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio_5 {
+
+	public static void main(String[] args) {
+		
+		Scanner miTeclado = new Scanner(System.in);
+	
+		int num0 = 0;
+		int num1 = 0;
+		int num2 = 0;
+		
+		System.out.println("Ingrese un primer numero");
+		num1 = miTeclado.nextInt();
+		
+		System.out.println("Ingrese un segundo numero");
+		num2 = miTeclado.nextInt();
+		
+		System.out.println("El primer numero es " + num1 + " y el segundo numero es " + num2);
+		num0 = num1;
+		num1 = num2;
+		num2 = num0;
+		
+		
+		System.out.println("El primer numero actualizado es " + num1 + " y el segundo numero actualizado es " + num2);
+		
+
+	}
+
+}
