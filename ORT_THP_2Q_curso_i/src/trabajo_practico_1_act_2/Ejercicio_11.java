84e8d6172c5da4e0ef250248e02daafcfb2ca450
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-12 19:45:38 -0300

    Ejercicio_11

---
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_2/Ejercicio_11.java
@@ -0,0 +1,28 @@
+package trabajo_practico_1_act_2;
+
+/*Enunciado
+Realizá un programa que permita ingresar un número entero e indique si se trata de un número par o impar. [EC]
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio_11 {
+
+	public static void main(String[] args) {
+	
+		Scanner miTeclado = new Scanner(System.in);
+		int num = 0;
+		
+		System.out.println("Ingrese un numero entero, le dire si es par o impar");
+		num =miTeclado.nextInt();
+		
+		if (num % 2 == 0) {
+            System.out.println(num + " es un número par");
+        } else {
+            System.out.println(num + " es un número impar");
+        }
+
+
+	}
+
+}
