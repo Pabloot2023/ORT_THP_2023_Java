f21c36a45578959f6e5dbbbc1cd7a7589179041c
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-12 15:33:25 -0300

    Ejercicio_4

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1/Ejercicio_4.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1/Ejercicio_4.java
@@ -7,11 +7,34 @@ package trabajo_practico_1;
   (Todas las horas valen lo mismo.)
  */
 
+import java.util.Scanner;
+
 public class Ejercicio_4 {
 
 	public static void main(String[] args) {
 		
-
+		Scanner miTeclado = new Scanner(System.in);
+		
+        double valorHora = 0;
+        double cantHora = 0;
+        double pagoDia = valorHora*cantHora;
+        double pagoSem = pagoDia*7;
+        double horaSab = ((cantHora/2)*valorHora);
+        
+        
+        System.out.println("Ingrese un numero el valor monetario de una hora de trabajo: ");
+        valorHora = miTeclado.nextDouble();
+        	
+        System.out.println("Ingrese la cantidad de horas trabajadas por dia del trabajador: ");
+        cantHora = miTeclado.nextDouble();
+        
+        System.out.println("\n" + "El valor del salario semanal es de:");
+        System.out.println("$ " + (pagoSem)) ;
+        
+        miTeclado.close();
+		
+       
+		
 	}
 
 }
