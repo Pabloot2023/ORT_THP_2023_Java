3e0459c46ae03a976866bbced3cd295e27ecfbc8
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-12 14:25:48 -0300

    Ejercicio_1

---
+++ ORT_THP_2Q_curso_i/src/tp1_actividad1/Ejercicio_1.java
@@ -0,0 +1,16 @@
+package tp1_actividad1;
+
+/*Enunciado
+ Realizá un programa que permita que el usuario ingrese su nombre.
+ El programa debe emitir una salida con un mensaje de bienvenida que incluya el nombre ingresado. [EC]
+ */
+
+
+public class Ejercicio_1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Bienvenidos a THP!!!!!!");
+		System.out.println("aprenderemos a programar muchas cosas!!!!!!");
+	}
+
+}
