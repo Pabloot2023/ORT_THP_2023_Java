22d09aa40b1b090849df15768dc6f57deb91cbc4
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-12 15:00:11 -0300

    Ejercicio_1

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1/Ejercicio_1.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1/Ejercicio_1.java
@@ -1,5 +1,7 @@
 package trabajo_practico_1;
 
+import java.util.Scanner;
+
 /*Enunciado
  Realizá un programa que permita que el usuario ingrese su nombre.
  El programa debe emitir una salida con un mensaje de bienvenida que incluya el nombre ingresado. [EC]
@@ -9,8 +11,12 @@ package trabajo_practico_1;
 public class Ejercicio_1 {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Bienvenidos a THP!!!!!!");
-		System.out.println("Aprenderemos a programar muchas cosas!!!!!!");
+		System.out.println("Aprenderemos a programar muchas cosas!!!!!");
+		
+		Scanner miTeclado = new Scanner(System.in);
+		miTeclado.close();
 	}
 
 }
