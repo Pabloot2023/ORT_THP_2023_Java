8962f3b01cd8de40dbd317f737c7405c3c5ba870
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-14 20:37:25 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_2/Ejercicio_11.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_2/Ejercicio_11.java
@@ -10,19 +10,19 @@ public class Ejercicio_11 {
 
 	public static void main(String[] args) {
 	
-		Scanner miTeclado1 = new Scanner(System.in);
+		Scanner miTeclado = new Scanner(System.in);
 		int num = 0;
 		
 		System.out.println("Ingrese un numero entero, le dire si es par o impar");
-		num =miTeclado1.nextInt();
+		num =Integer.parseInt(miTeclado.nextLine());
 		
 		if (num % 2 == 0) {
-            System.out.println(num + " es un número par");
+            System.out.println("\n" + num + " es un numero par");
         } else {
-            System.out.println(num + " es un número impar");
+            System.out.println("\n" + num + " es un numero impar");
         }
 
-		  miTeclado1.close();
+		  miTeclado.close();
 	}
 
 }
