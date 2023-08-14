0a0d19a9c5bf7cb375009fd1fabb229f00a2183d
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-14 19:45:41 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_1/Ejercicio_6.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_1/Ejercicio_6.java
@@ -14,13 +14,15 @@ public class Ejercicio_6 {
 	
 	Scanner miTeclado = new Scanner(System.in);
 	
+	final String CARTEL = "Bienvenido";
 	int ventasTot = 0;
-	int sueldoFijo = 44000;
-	double comision = 0.16;
+	final int SUELDOFIJO = 44000;
+	final double COMISION = 16;
 	
+	System.out.println(CARTEL);
 	System.out.println("Ingrese el monto total de las ventas realizadas");
 	ventasTot = miTeclado.nextInt();
-	System.out.println("\n" + "El monto a cobrar por el vendedor es de: $" + ((double)sueldoFijo+(ventasTot*comision)));
+	System.out.println("\n" + "El monto a cobrar por el vendedor es de: $" + ((double)SUELDOFIJO+(ventasTot*COMISION/100)));
 	
 	miTeclado.close();
 	
