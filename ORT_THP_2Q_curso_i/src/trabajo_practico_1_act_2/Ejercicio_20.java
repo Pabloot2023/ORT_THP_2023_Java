d11c77b25cc980caaebced6ce1e4afc37be1dac0
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-14 22:45:54 -0300

    .

---
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_2/Ejercicio_20.java
@@ -0,0 +1,63 @@
+package trabajo_practico_1_act_2;
+
+/*Enunciado
+ * Realizá un programa que permita ingresar dos números enteros 
+ * y la operación a realizar (' + ', -', '”’, / ). 
+ * Debe mostrarse el resultado para la operación ingresada.
+ *  Considerar que no se puede dividir por cero (en ese caso mostrar el texto ERROR ' ). [EC]
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio_20 {
+
+	public static void main(String[] args) {
+		Scanner miTeclado = new Scanner(System.in);
+		
+		 int num1;
+		 int num2;
+		 int resultado;
+	     char operacion;
+
+	    System.out.print("Ingrese el primer número: ");
+	    num1 = miTeclado.nextInt();
+
+	    System.out.print("Ingrese el segundo número: ");
+	    num2 = miTeclado.nextInt();
+
+	    System.out.print("Ingrese la operación a realizar (+, -, *, /): ");
+	    operacion = miTeclado.next().charAt(0);
+
+	        switch (operacion) {
+	            case '+':
+	                resultado = num1 + num2;
+	                System.out.println("\n" + "El resultado de la suma es: " + resultado);
+	                break;
+	            case '-':
+	                resultado = num1 - num2;
+	                System.out.println("\n" + "El resultado de la resta es: " + resultado);
+	                break;
+	            case '*':
+	                resultado = num1 * num2;
+	                System.out.println("\n" + "El resultado de la multiplicación es: " + resultado);
+	                break;
+	            case '/':
+	                if (num2 == 0) {
+	                    System.out.println("\n" + "ERROR: No se puede dividir por cero.");
+	                } else {
+	                    resultado = num1 / num2;
+	                    System.out.println("\n" + "El resultado de la división es: " + resultado);
+	                }
+	                break;
+	            default:
+	                System.out.println("ERROR: Operación inválida.");
+	                break;
+	        }
+
+	        miTeclado.close();
+		
+		
+
+	}
+
+}
