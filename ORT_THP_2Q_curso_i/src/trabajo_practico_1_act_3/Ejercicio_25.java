46f238ba1628ace5e4e28deaaf851f0a100a5c19
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-26 00:52:35 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_25.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_25.java
@@ -6,11 +6,37 @@
 
 package trabajo_practico_1_act_3;
 
+import java.util.Scanner;
+
 public class Ejercicio_25 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner miTeclado = new Scanner(System.in);
 
-	}
+        System.out.print("Ingrese el ancho de la matriz de cruces: ");
+        int ancho = miTeclado.nextInt();
 
+        System.out.print("Ingrese el alto de la matriz de cruces: ");
+        int alto = miTeclado.nextInt();
+        
+        
+
+        // Dibujo optimizado de la matriz de cruces
+        for (int i = 0; i < alto; i++) {
+
+            for (int j = 0; j < ancho; j++) {
+            	
+                if (j == ancho / 2 && i == alto / 2) {
+                    System.out.print("+ ");
+                } else if (j == 0 || j == ancho - 1 || i == 0 || i == alto - 1 ) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        miTeclado.close();
+    }
 }
