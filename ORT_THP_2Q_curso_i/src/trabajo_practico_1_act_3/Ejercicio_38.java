a9920c1f0de378b2fa31f7f10b56ae51a97f1286
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-25 21:05:44 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_38.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_38.java
@@ -1,3 +1,13 @@
+/*Enunciado
+38.	Realizá un programa que permita controlar con validación el ingreso a un sitio
+ web. Teniendo ya precargados un nombre de usuario ("admin") 
+ y una contraseña ("123456"), 
+ el programa debe permitir al usuario ingresar sus credenciales. 
+ Si las mismas son erróneas, se volverán a pedir hasta un máximo de 3 intentos. 
+ Finalmente, la computadora debe mostrar alguno de los siguientes mensajes según 
+ sea el caso: "Acceso concedido" o "Se ha bloqueado la cuenta”.
+*/
+
 package trabajo_practico_1_act_3;
 
 public class Ejercicio_38 {
--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_39.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_39.java
@@ -1,3 +1,26 @@
+/*Enunciado
+39.	Nos proponemos desarrollar un “Tiro al blanco” en el cual 2 o más participantes realizan 3 disparos consecutivos cada uno.
+ Cada tiro, dependiendo de la distancia al centro da un puntaje que se acumula (se suman los tres disparos).
+  Gana el jugador con mayor puntaje (se supone único).
+  
+La puntuación para cada tiro es la siguiente:
+•	Si la distancia respecto al centro es 0 se ganan 500 puntos;
+•	Si la distancia es <= a 10, se ganan 250;
+•	Si la distancia está entre 11y 50 ganará 100 puntos;
+•	Si es mayor no ganará nada (cayó fuera del tablero).
+
+La mecánica del juego es la siguiente:
+
+Se pide la cantidad de jugadores (mayor o igual a 2). Por cada jugador:
+•	Ingresar el nombre
+•	Ingresar la distancia de cada uno de sus tres tiros consecutivos (número mayor o igual a cero), calculando para cada uno de estos el puntaje obtenido.
+
+Se pide:
+•	Informar el nombre del participante ganador del torneo y su puntaje (suponer único
+máximo)
+•	Informar la cantidad total de tiros al centro.
+*/
+
 package trabajo_practico_1_act_3;
 
 public class Ejercicio_39 {
