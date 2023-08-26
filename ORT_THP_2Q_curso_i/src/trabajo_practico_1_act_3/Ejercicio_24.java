f324e793d9673dc14414b19c8e0339e60d4defc6
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-26 00:04:12 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_24.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_24.java
@@ -19,6 +19,32 @@ public class Ejercicio_24 {
 		
 		int num1;
 		int num2;
+		int i;
+		
+		System.out.print("Ingrese un numero entero: ");
+        num1 = miTeclado.nextInt();
+        
+        System.out.print("Ingrese otro numero entero: ");
+        num2 = miTeclado.nextInt();
+        
+        /*
+         * 
+         * ver que forma de ciclo usar para validar que num 2 siempre sea menor a num 1
+         * 
+         */
+        System.out.println("--------------------------------------");
+        System.out.println("\n" + "La secuencia de numeros entre " + num1 + " y " + num2 + " incluyendolos, es:" );
+        for (i = num1 ; i >= num2 ; i--) {
+            System.out.println("--> " + i);
+        }
+        System.out.println("\n" + "--------------------------------------");
+        System.out.println("\n" + "La secuencia de numeros entre " + num1 + " y " + num2 + " excluyendolos, es:" );
+        for (i = num1 - 1 ; i > num2  ; i--) {
+            System.out.println("--> " + i);
+        }
+        System.out.println("\n" + "--------------------------------------");
+        
+        
 
 	}
 
