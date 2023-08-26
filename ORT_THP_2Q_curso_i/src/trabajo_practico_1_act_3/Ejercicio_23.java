1b4c23b0c164597b12f3da2bb14b4c88ed46a1b1
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-25 21:55:07 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_23.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_23.java
@@ -1,14 +1,29 @@
 /*Enunciado
-	23.	Realizá un programa que permita ingresar un número entero n.
+	Realizá un programa que permita ingresar un número entero n.
 	 Debe mostrar los primeros 10 múltiplos de n (desde 1 x n).
 */
 
 package trabajo_practico_1_act_3;
 
+import java.util.Scanner;
+
 public class Ejercicio_23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner miTeclado = new Scanner(System.in);
+		int n;
+		int multiplo;
+		
+		System.out.print("Ingrese un numero entero: ");
+         n = miTeclado.nextInt();
+         
+         System.out.println("Los primeros 10 multiplos de " + n + " son:");
+         for (int i = 1; i <= 10; i++) {
+             multiplo = n * i;
+             System.out.println("--> " + multiplo);
+             
+         miTeclado.close();    
+         }
 
 	}
 
