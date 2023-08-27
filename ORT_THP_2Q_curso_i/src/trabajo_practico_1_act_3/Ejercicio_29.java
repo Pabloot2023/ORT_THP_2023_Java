064891632c6094646a43d7b23cb893ff060e73fe
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-26 21:31:31 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_29.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_29.java
@@ -7,11 +7,27 @@ como sea necesario hasta que quede comprendida dentro del rango indicado. [EC]
 
 package trabajo_practico_1_act_3;
 
+import java.util.Scanner;
+
 public class Ejercicio_29 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner miTeclado = new Scanner(System.in);
+		int i;
+		int nota;
+		
+		System.out.println("Ingrese la nota del examen:");
+		nota = miTeclado.nextInt();
+		
+		 for ( i = 0; nota < 0 || nota > 10; i++) {
+			 System.out.println("\n" + "-----------------ERROR-------------------");
+			 System.out.println("Solamente es valida del 1 al 10");
+			 System.out.println("Reingrese la nota  del examen:");
+			 nota = miTeclado.nextInt();
+		 }
+		 	System.out.println("\n" + "-----------------EXITO!!-------------------");
+			System.out.println("\n" + "             Nota almacenada");
+			System.out.println("\n" + "------------PROGRAMA FINALIZADO------------");
 	}
 
 }
