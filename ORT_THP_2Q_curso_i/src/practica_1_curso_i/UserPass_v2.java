1b8da2cf8f338b9f468d4ccea91f3a46a8a1ef02
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-28 21:32:33 -0300

    .

---
+++ ORT_THP_2Q_curso_i/src/practica_1_curso_i/UserPass_v2.java
@@ -0,0 +1,38 @@
+package practica_1_curso_i;
+
+import java.util.Scanner;
+
+public class UserPass_v2 {
+
+	public static void main(String[] args) {
+		
+		Scanner miTeclado = new Scanner(System.in);
+		
+		final String USUARIO_VALIDO = "PEPE";
+		final String CONTRASENIA_VALIDA = "12342023";
+		
+		String usuarioIngresado;
+		String claveIngresada;
+		
+		
+		
+			do {
+		
+				System.out.println("Ingrese su Usuario");
+				usuarioIngresado = miTeclado.nextLine();
+		
+				System.out.println("Ingrese su Contrasenia");
+				claveIngresada = miTeclado.nextLine();
+		
+				} while ( usuarioIngresado.equalsIgnoreCase(USUARIO_VALIDO) || claveIngresada.equals(CONTRASENIA_VALIDA));
+		
+		if (usuarioIngresado.equalsIgnoreCase(USUARIO_VALIDO) && claveIngresada.equals(CONTRASENIA_VALIDA)) {
+			System.out.println("Bienvenido");
+		} else {
+			System.out.println("Datos incorrecto, no puede acceder!!!");
+		}
+				
+
+	}
+
+}
