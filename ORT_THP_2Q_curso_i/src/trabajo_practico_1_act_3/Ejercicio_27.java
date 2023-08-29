d6f731a32031e80c58293b1b00d9fa95f60f51c7
Pabloot2023 <pablo32laboral@gmail.com>
2023-08-29 20:39:21 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_27.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_27.java
@@ -10,8 +10,6 @@ import java.util.Scanner;
 
 public class Ejercicio_27 {
 	
-	
-
 	public static void main(String[] args) {
 		Scanner miTeclado = new Scanner(System.in);
 		
@@ -29,15 +27,14 @@ public class Ejercicio_27 {
 		        	mayor_18++;
 		        } 
 		        edad_acum += edad_num;
-		 } 
+		        
+		 	  } 
 		 
 		 promedio = edad_acum / 5;
 		 System.out.println("--------------------------------------");
 		 System.out.print("El valor promedio de Edad es: " + promedio);
 		 System.out.print("\n" + "La cantidad de edades impares y mayores a 18 es: " + mayor_18);
 		 System.out.println("\n" + "--------------------------------------");
-		
-
 	}
 
 }
