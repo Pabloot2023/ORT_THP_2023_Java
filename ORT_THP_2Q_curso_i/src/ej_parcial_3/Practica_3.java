479cea74fe9e5461067cc5af564e43909d7c8bf1
Pabloot2023 <pablo32laboral@gmail.com>
2023-09-04 23:23:31 -0300

    .

--- ORT_THP_2Q_curso_i/src/ej_parcial_3/Practica_3.java
+++ ORT_THP_2Q_curso_i/src/ej_parcial_3/Practica_3.java
@@ -3,15 +3,15 @@ package ej_parcial_3;
 import java.util.Scanner;
 
 public class Practica_3 {
-	public static void main(String[] args) {
-        Scanner  miTeclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner miTeclado = new Scanner(System.in);
 
         final int VUELTAS_TOTALES = 5;
         int vueltasRojo = 0;
         int vueltasVerde = 0;
         int pianitosRojo = 0;
-        int acum_pianitosRojo =0;
-        int acum_pianitosVerde =0;
+        int acum_pianitosRojo = 0;
+        int acum_pianitosVerde = 0;
         int pianitosVerde = 0;
         boolean carreraTerminada = false;
         boolean finalAbrupto = false;
@@ -19,85 +19,75 @@ public class Practica_3 {
         double promedioRojo;
         double promedioVerde;
         String kartingMenosPianitos;
-        
-        
+
         do {
-        	 
-        	
-        	while (!Karting.equals("R") && !Karting.equals("V") && !Karting.equals("F")) {
-        	    System.out.println("Informe el karting (R/V) o F si es que debieron suspender la carrera");
-        	    Karting = miTeclado.nextLine().toUpperCase();
-        	}
+            while (!Karting.equals("R") && !Karting.equals("V") && !Karting.equals("F")) {
+                System.out.println("Informe el karting (R/V) o F si es que debieron suspender la carrera");
+                Karting = miTeclado.nextLine().toUpperCase();
+            }
 
+            if (Karting.equals("R")) {
+                vueltasRojo++;
+                System.out.println("Ingrese la cantidad de Pianitos para el Karting Rojo");
+                pianitosRojo = miTeclado.nextInt();
+                miTeclado.nextLine(); // Consumir el carácter de nueva línea
+                while (pianitosRojo < 0) {
+                    System.out.println("ERROR: Ingrese la cantidad de Pianitos para el Karting Rojo igual o mayor a 0");
+                    pianitosRojo = miTeclado.nextInt();
+                    miTeclado.nextLine(); // Consumir el carácter de nueva línea
+                }
+            }
 
+            if (Karting.equals("V")) {
+                vueltasVerde++;
+                System.out.println("Ingrese la cantidad de Pianitos para el Karting Verde");
+                pianitosVerde = miTeclado.nextInt();
+                miTeclado.nextLine(); // Consumir el carácter de nueva línea
+                while (pianitosVerde < 0) {
+                    System.out.println("ERROR: Ingrese la cantidad de Pianitos para el Karting Verde igual o mayor a 0");
+                    pianitosVerde = miTeclado.nextInt();
+                    miTeclado.nextLine(); // Consumir el carácter de nueva línea
+                }
+            }
 
-		        	
+            if (Karting.equals("F") || Karting.equals("f")) {
+                finalAbrupto = true;
+            }
 
-					            if (Karting.equals("V")) {
-					                vueltasRojo++;
-					                do {
-					                    System.out.println("Ingrese la cantidad de Pianitos para el Karting Verde");
-					                    pianitosRojo = miTeclado.nextInt();
-					                } while (pianitosRojo < 0);
-					            }
+            if (vueltasRojo >= VUELTAS_TOTALES || vueltasVerde >= VUELTAS_TOTALES) {
+                carreraTerminada = true;
+            }
 
+            // Reiniciar Karting para la próxima iteración
+            Karting = "a";
 
-					
-					            if (Karting.equals("V")) {
-					                vueltasVerde++;
-					                do {
-					                    System.out.println("Ingrese la cantidad de Pianitos para el Karting Verde");
-					                    pianitosVerde = miTeclado.nextInt();
-					                } while (pianitosVerde < 0);
-					            }
-            
-					            
-            
-        	    if (Karting.equals("F") || Karting.equals("f")) {
-        	        finalAbrupto = true;
-        	    }
-
-        	    if (vueltasRojo >= VUELTAS_TOTALES || vueltasVerde >= VUELTAS_TOTALES) {
-        	        carreraTerminada = true;
-        	    }
-
-        	 
         } while (!carreraTerminada && !finalAbrupto);
-        
+
         promedioRojo = (double) pianitosRojo / vueltasRojo;
         promedioVerde = (double) pianitosVerde / vueltasVerde;
-        
-        
+
         if (promedioRojo < promedioVerde) {
             kartingMenosPianitos = "Rojo!";
         } else {
             kartingMenosPianitos = "Verde!";
         }
-        
-        
-             
-        if ((vueltasRojo > vueltasVerde) && !finalAbrupto) {
-        	System.out.println("Karting ganador: Rojo!");
-        	System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
-        	    	
-        }
-        
-        if ((vueltasVerde > vueltasRojo) && !finalAbrupto) {
-        	System.out.println("Karting ganador: Verde!");
-        	System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
-        	    	
-        }
-        
-        if (finalAbrupto) {
-        	 kartingMenosPianitos = "Sin datos por carrera suspendida";
-        	System.out.println("Carera suspendida, FINAL ABRUPTO!");
-        	System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
-        	    	
-        }
-        
-        miTeclado.close(); 
-        
-        
 
+        if ((vueltasRojo > vueltasVerde) && !finalAbrupto) {
+            System.out.println("Karting ganador: Rojo!");
+            System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
+        }
+
+        if ((vueltasVerde > vueltasRojo) && !finalAbrupto) {
+            System.out.println("Karting ganador: Verde!");
+            System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
+        }
+
+        if (finalAbrupto) {
+            kartingMenosPianitos = "Sin datos por carrera suspendida";
+            System.out.println("Carrera suspendida, FINAL ABRUPTO!");
+            System.out.println("Karting con menos pianitos en promedio fue: " + kartingMenosPianitos);
+        }
+
+        miTeclado.close();
     }
 }
