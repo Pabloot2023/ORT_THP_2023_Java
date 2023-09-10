972874f31b584e6e15223e274e5e6ab387941650
Pabloot2023 <pablo32laboral@gmail.com>
2023-09-10 17:34:07 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_33.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_33.java
@@ -6,11 +6,44 @@
 
 package trabajo_practico_1_act_3;
 
+import java.util.Scanner;
+
 public class Ejercicio_33 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner miTeclado = new Scanner(System.in);
+		
+		int num_max = 0;
+		int num_min = 100000;
+		int num;
+		
+		
+		
+		 System.out.println("ingrese la nota  del examen:");
+		 num = miTeclado.nextInt();
+		
+				while ( num != 0) {
+					
+					 
+							 if (num > num_max) {
+								 num_max = num;
+							 }
+							 
+							 if (num < num_min) {
+								 num_min = num;
+							 }
+							 
+							 System.out.println("ingrese la nota  del examen:");
+							 num = miTeclado.nextInt();
+		
+				 }
+		System.out.println("\n" + "------------------------------------");		
+		System.out.println("La nota mas alta fue: " + num_max);		
+		
+		System.out.println("\n" + "La nota mas baja fue: " + num_min);	
+		System.out.println("------------------------------------");	
+		
+		miTeclado.close();
 	}
 
 }
