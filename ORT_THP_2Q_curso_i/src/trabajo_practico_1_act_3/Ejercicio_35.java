cee0e7472d45a316750e765cb1f79f7f9e26dcd4
Pabloot2023 <pablo32laboral@gmail.com>
2023-09-10 18:11:22 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_35.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_1_act_3/Ejercicio_35.java
@@ -2,16 +2,48 @@
 35.	Realizá un programa que permita ingresar 
 nombre y edad de un grupo de personas (para cada una, nombre y edad).
  La carga termina cuando en el nombre de la persona se ingresa un asterisco 
- (’ ” '). Mostrar al final cómo se Ilama la persona más joven.
+ (’ * '). Mostrar al final cómo se Ilama la persona más joven.
 */
 
 package trabajo_practico_1_act_3;
 
+import java.util.Scanner;
+
 public class Ejercicio_35 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner miTeclado1 = new Scanner(System.in);
+        Scanner miTeclado2 = new Scanner(System.in);
 
-	}
+        String nombre;
+        int edad;
+        int younger = 100000;
+        String dato_nombre = "a";
+        int dato_edad = 0;
 
+        System.out.print("Ingrese el nombre de la persona: ");
+        nombre = miTeclado1.nextLine();
+
+        while (!nombre.equals("*")) {
+
+            System.out.print("Ingrese la edad de la persona: ");
+            edad = miTeclado2.nextInt();
+
+            if (edad < younger) {
+                dato_nombre = nombre;
+                dato_edad = edad;
+            }
+            
+            nombre = "a";
+            edad = 0;
+
+            System.out.print("Ingrese el nombre de la persona: ");
+
+            nombre = miTeclado1.nextLine();
+        }
+
+        System.out.println("El nombre de la persona mas Joven es: " + dato_nombre);
+        miTeclado1.close();
+        miTeclado2.close();
+    }
 }
