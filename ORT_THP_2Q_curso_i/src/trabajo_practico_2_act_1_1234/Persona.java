4eafea974c2349dc0661034c2500da7a081681a0
Pabloot2023 <pablo32laboral@gmail.com>
2023-09-26 20:16:12 -0300

    .

---
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_2_act_1_1234/Persona.java
@@ -0,0 +1,27 @@
+package trabajo_practico_2_act_1_1234;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+
+    // Constructor por defecto
+    public Persona() {
+        this.nombre = "";
+        this.apellido = "";
+    }
+
+    // Método para establecer el nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Método para establecer el apellido
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Método para obtener el nombre completo
+    public String obtenerNombreCompleto() {
+        return this.nombre + " " + this.apellido;
+    }
+}
---
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_2_act_1_1234/Test.java
@@ -0,0 +1,25 @@
+package trabajo_practico_2_act_1_1234;
+
+public class Test {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Persona
+        Persona persona1 = new Persona();
+
+        // Asignar el nombre "Fulano" a la primera persona
+        persona1.setNombre("Fulano");
+        // Asignar el apellido "Gomez" a la primera persona
+        persona1.setApellido("Gomez");
+        // Mostrar el nombre completo de la primera persona
+        System.out.println("Nombre completo de la primera persona: " + persona1.obtenerNombreCompleto());
+
+        // Crear otra instancia de la clase Persona
+        Persona persona2 = new Persona();
+        // Asignar el nombre "Sutano" a la segunda persona
+        persona2.setNombre("Sutano");
+        // Asignar el apellido "Perez" a la segunda persona
+        persona2.setApellido("Perez");
+        // Mostrar el nombre completo de la segunda persona
+        System.out.println("Nombre completo de la segunda persona: " + persona2.obtenerNombreCompleto());
+        
+    }
+}
