42a2891f8d3f953c3d27ef22acb79e95fd41e663
Pabloot2023 <pablo32laboral@gmail.com>
2023-10-03 18:50:30 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_2_act_1_Ejs13/SuperHeroe.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_2_act_1_Ejs13/SuperHeroe.java
@@ -48,17 +48,23 @@ public class SuperHeroe {
     private int validarPoder(int poder) {
         if (poder < 0) {
             return 0;
+            
         } else if (poder > 100) {
             return 100;
+            
         } else {
             return poder;
         }
     }
 
     public String competir(SuperHeroe otroSuperHeroe) {
-        int propioPoder = (fuerza + resistencia + superpoderes) / 3;
-        int otroPoder = (otroSuperHeroe.getFuerza() + otroSuperHeroe.getResistencia() + otroSuperHeroe.getSuperpoderes()) / 3;
-
+        int propioPoder;
+        int otroPoder;
+        
+        propioPoder = (fuerza + resistencia + superpoderes) / 3;
+        
+        otroPoder = (otroSuperHeroe.getFuerza() + otroSuperHeroe.getResistencia() + otroSuperHeroe.getSuperpoderes()) / 3;
+        
         if (propioPoder - otroPoder >= 2) {
             return "TRIUNFO";
         } else if (otroPoder - propioPoder >= 2) {
