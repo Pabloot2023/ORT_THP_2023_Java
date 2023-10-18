fb8c256cad89cb24df89eeea76348a3919198d90
Pabloot2023 <pablo32laboral@gmail.com>
2023-10-17 22:37:08 -0300

    .

--- ORT_THP_2Q_curso_i/src/trabajo_practico_3_Ej_3/Barrio.java
+++ ORT_THP_2Q_curso_i/src/trabajo_practico_3_Ej_3/Barrio.java
@@ -2,6 +2,8 @@ package trabajo_practico_3_Ej_3;
 
 import java.util.ArrayList;
 
+import trabajo_practico_3_Ej_2.Empleado;
+
 class Barrio {
     private String nombre;
     private ArrayList<Propiedad> propiedades;
@@ -15,9 +17,31 @@ class Barrio {
         return propiedades;
     }
 
-    public void agregarPropiedad(Propiedad propiedad) {
-        propiedades.add(propiedad);
+    public boolean agregarPropiedad(Propiedad propiedad) {
+        boolean pudo = false;
+        Propiedad propiedadNueva = buscarPropiedad(propiedad.obtenerDomicilio(), propiedad.obtenerPrecio(), propiedad.obtenerTipo());
+        if (propiedadNueva == null) { // No lo encontró, entonces podemos crearlo
+            this.propiedades.add(propiedad);
+            pudo = true;
+        }
+        return pudo;
     }
+    
+    private Propiedad buscarPropiedad(String domicilio, double precio, tipoPropiedad tipo) {
+        Propiedad propiedadBuscada = null;
+        int i = 0;
+        while (i < this.propiedades.size() && propiedadBuscada == null) {
+            if (this.propiedades.get(i).obtenerDomicilio().equals(domicilio) &&
+                this.propiedades.get(i).obtenerPrecio() == precio &&
+                this.propiedades.get(i).obtenerTipo() == tipo) {
+                propiedadBuscada = this.propiedades.get(i);
+            } else {
+                i++;
+            }
+        }
+        return propiedadBuscada;
+    }
+    
 
     public void mostrarPropiedades(String tipo) {
         System.out.println("Propiedades en el barrio " + nombre + " de tipo " + tipo + ":");
